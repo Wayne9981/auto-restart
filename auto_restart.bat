ac6c9795125db54f9bf1a144407836a9aaa1db03
Wayne9981 <86971349+Wayne9981@users.noreply.github.com>
2025-07-21 19:42:33 +0800

    Modify language

--- auto_restart.bat
+++ auto_restart.bat
@@ -1,16 +1,16 @@
 @echo off
-REM 自動重開機腳本 - 開機後三分鐘執行
-REM 作者：自動生成
-REM 建立日期：2025年7月21日
+REM Auto Restart Script - Executes after a delay
+REM Author: Auto-generated
+REM Created on: July 21, 2025
 
-echo 系統將在3分鐘後自動重開機...
-echo 開始時間：%date% %time%
+echo The system will automatically restart in 3 minutes...
+echo Start time: %date% %time% >> C:\Windows\Temp\auto_restart_log.txt
 
-REM 等待180秒（3分鐘）
+REM Wait for 180 seconds (3 minutes)
 timeout /t 180 /nobreak
 
-REM 記錄重開機時間
-echo 重開機時間：%date% %time% >> C:\Windows\Temp\auto_restart_log.txt
+REM Log the restart time
+echo Restart time: %date% %time% >> C:\Windows\Temp\auto_restart_log.txt
 
-REM 執行重開機命令
-shutdown /r /t 0 /c "自動重開機腳本執行完成"
+REM Execute the restart command
+shutdown /r /t 0 /c "Auto restart script executed"
