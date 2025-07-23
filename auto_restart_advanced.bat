572dd0b25dc629f126698d286db8ac5c6155f99e
junwei <junwei@actionapp.io>
2025-07-23 18:16:12 +0800

    Add Configuration Block

--- auto_restart_advanced.bat
+++ auto_restart_advanced.bat
@@ -7,6 +7,15 @@ REM Creation Date: July 21, 2025
 setlocal enabledelayedexpansion
 title Automatic Restart Script
 
+REM ========================================
+REM           Configuration Block
+REM ========================================
+set "COUNT_FILE=C:\Windows\Temp\auto_restart_count.txt"
+set "LOG_FILE=C:\Windows\Temp\auto_restart_log.txt"
+set "COUNTDOWN_SECONDS=180"
+set "SHUTDOWN_DELAY_SECONDS=5"
+REM ========================================
+
 REM Check Administrator Privileges
 net session >nul 2>&1
 if %errorLevel% neq 0 (
@@ -16,30 +25,27 @@ if %errorLevel% neq 0 (
     exit /b 1
 )
 
-REM Define the log file for restart count
-set "restart_count_log=C:\Windows\Temp\auto_restart_count.txt"
-
 REM Initialize restart count
-if not exist "%restart_count_log%" (
-    echo 0 > "%restart_count_log%"
+if not exist "%COUNT_FILE%" (
+    echo 0 > "%COUNT_FILE%"
 )
 
 REM Read current restart count
-set /p restart_count=<"%restart_count_log%"
+set /p restart_count=<"%COUNT_FILE%"
 set /a restart_count=!restart_count! + 1
-echo !restart_count! > "%restart_count_log%"
+echo !restart_count! > "%COUNT_FILE%"
 
 echo.
 echo ========================================
 echo        Automatic Restart Script
 echo ========================================
 echo.
-echo The system will automatically restart in 3 minutes.
+echo The system will automatically restart in %COUNTDOWN_SECONDS%/60 minutes %COUNTDOWN_SECONDS%%%60 seconds.
 echo If you wish to cancel, press Ctrl+C during the countdown.
 echo.
 
-REM Countdown (180 seconds = 3 minutes)
-for /L %%i in (180,-1,1) do (
+REM Countdown
+for /L %%i in (%COUNTDOWN_SECONDS%,-1,1) do (
     set /a minutes=%%i/60
     set /a seconds=%%i%%60
     echo Remaining time: !minutes! min !seconds! sec
@@ -50,12 +56,10 @@ echo.
 echo Preparing to restart...
 
 REM Log Restart Time
-set restart_time=%date% %time%
-echo Restart Execution Time：!restart_time!
-echo [!restart_time!] Restart count: !restart_count! >> C:\Windows\Temp\auto_restart_log.txt
-
+echo [%date% %time%] Restart count: !restart_count!
+echo [%date% %time%] Restart count: !restart_count! >> "%LOG_FILE%"
 
 REM Execute Restart
-shutdown /r /t 5 /c "Automatic Restart Script - The system will restart in 5 seconds."
-echo The restart command has been executed, the system will restart in 5 seconds....
-timeout /t 5 /nobreak >nul
+shutdown /r /t %SHUTDOWN_DELAY_SECONDS% /c "Automatic Restart Script - The system will restart in %SHUTDOWN_DELAY_SECONDS% seconds."
+echo The restart command has been executed, the system will restart in %SHUTDOWN_DELAY_SECONDS% seconds....
+timeout /t %SHUTDOWN_DELAY_SECONDS% /nobreak >nul
